6d8af6ad89614b7f224d6691c70c3e93c8138ac0
hayashisouta <hayashisouta@10.82.2.154>
2025-05-12 11:53:42 +0900

    最初のコミット

---
+++ src/sample/JavaSampleClass.java
@@ -0,0 +1,10 @@
+package sample;
+public class JavaSampleClass {
+    public static void greet(String[] args) {
+        System.out.println("こんにちは！Javaプログラミングの世界へようこそ！");
+    }
+    public static void main(String[] args) {
+        greet(args);
+    }
+}
+
